93183514b87f4329d33cf63f1d25fa435b2e9f32
seven <csh7@tju.edu.cn>
2018-08-22 22:59:48 +0800

    first

---
+++ shuangpin_coach/coach.py
@@ -0,0 +1,12 @@
+class couch:
+
+    sheng_mu = ['b','p','m','f','d',
+                't', 'n', 'l', 'g', 'k',
+                'h', 'j', 'q', 'x', 'zh',
+                'ch', 'sh', 'r', 'z', 'c',
+                's', 'y', 'w']
+    yun_mu = ['ua','ei','e','ou','iu','ve','u','i','o','uo','ie',
+              'a','ong','iong','ai','ing','uai','ang','uan','an','en','ia','iang','uang',
+              'eng','in','ao','v','ui','un','iao','ian']
+
+     
