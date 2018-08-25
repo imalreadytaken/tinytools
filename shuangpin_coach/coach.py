ee76cb59f776ac55ac5c5be1288604f6b2079df0
seven <csh7@tju.edu.cn>
2018-08-25 22:30:44 +0800

    version 0.1

--- shuangpin_coach/coach.py
+++ shuangpin_coach/coach.py
@@ -1,24 +1,39 @@
+import random
+
 class couch:
-
-    sheng_mu = ['b','p','m','f','d',
-                't', 'n', 'l', 'g', 'k',
-                'h', 'j', 'q', 'x', 'zh',
-                'ch', 'sh', 'r', 'z', 'c',
-                's', 'y', 'w']
-    yun_mu = ['ua','ei','e','ou','iu','ve','u','i','o','uo','ie',
-              'a','ong','iong','ai','ing','uai','ang','uan','an','en','ia','iang','uang',
-              'eng','in','ao','v','ui','un','iao','ian']
-
     codes = {
-        'natural' : {
-            'sheng_mu': ['b','p','m','f','d',
-                         't', 'n', 'l', 'g', 'k',
-                         'h', 'j', 'q', 'x', 'zh',
-                         'ch', 'sh', 'r', 'z', 'c',
-                         's', 'y', 'w'],
-            'yun_mu': ['ua','ei','e','ou','iu','ve','u','i','o','uo','ie',
-                        'a','ong','iong','ai','ing','uai','ang','uan','an','en','ia','iang','uang',
-                        'eng','in','ao','v','ui','un','iao','ian'],
+        'natural': {
+            'sheng_mu': {
+                'q': 'q', 'w': 'w',
+                'r': 'r',
+                't': 't', 'y': 'y',
+                'sh': 'u', 'ch': 'i', 'un': 'p',
+                's': 's',
+                'd': 'd',
+                'f': 'f', 'g': 'g',
+                'h': 'h', 'j': 'j',
+                'k': 'k', 'l': 'l',
+                'z': 'z', 'x': 'x',
+                'c': 'c', 'zh': 'v',
+                'b': 'b', 'n': 'n',
+                'm': 'm'
+            },
+            'yun_mu': {
+                'iu': 'q', 'ua': 'w', 'ia': 'w',
+                'e': 'e', 'uan': 'r', 'van': 'r',
+                'ue': 't', 've': 't', 'uai': 'y', 'ing': 'y',
+                'u': 'u', 'i': 'i',
+                'uo': 'o', 'o': 'o', 'un': 'p', 'vn': 'p',
+                'a': 'a', 'iong': 's', 'ong': 's',
+                'uang': 'd', 'iang': 'd',
+                'en': 'f', 'eng': 'g',
+                'ang': 'h', 'an': 'j',
+                'ao': 'k', 'ai': 'l',
+                'ei': 'z', 'ie': 'x',
+                'iao': 'c', 'v': 'v', 'ui': 'v',
+                'ou': 'b', 'in': 'n',
+                'ian': 'm'
+            },
         }
     }
 
@@ -35,8 +50,14 @@ class couch:
 
     def test(self):
         self.choose_code()
-        print(self.code_selected)
-
+        while True:
+            answer = random.sample(self.code_selected['yun_mu'].keys(), 1)[0]
+            print('please insert code for : ',answer)
+            a = input()
+            while a != self.code_selected['yun_mu'][answer]:
+                print('wrong! try again : ')
+                a = input()
+            print('good job!')
 
 if __name__ == '__main__':
     couch().test()
